9e1e9328ead3e93add29d239ff1bcf25129a7940
charleskoko <55494612+charleskoko@users.noreply.github.com>
2020-04-15 03:03:30 +0200

    hh

---
+++ TankAufgabe/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace TankAufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+            Tank diesel = new Tank {volume = 1000, inhalt=0};
+
+            do
+            {
+                Console.Clear();
+                choice = Menu(diesel);
+                Console.Clear();
+                tankLeiter(choice, diesel);
+
+            } while (choice != 'x');
+         
+        }
+
+
+
+
+
+
+
+        public static void tankLeiter(char choice, Tank tank)
+        {
+            switch (choice)
+            {
+                case 'b':
+                    Console.Clear();
+                    Console.Write("Wieviel Liter moechten Sie einfuellen: "); 
+                    int userEingabe = UserEingabeChecking();
+                    tank.Befuellen(userEingabe);
+                    break;
+                case 'e':
+                    Console.Clear();
+                    Console.Write("Wieviel Liter moechten Sie entnehmen: ");
+                    userEingabe = UserEingabeChecking();
+                    tank.Entnehmen(userEingabe);
+                    break;
+            }
+        }
+
+        public static void DispLayMenu(Tank tank)
+        {
+            Console.WriteLine("Tankvolumen: "+ tank.volume +" Liter");
+            Console.WriteLine("Tankinhalt: "+ tank.inhalt+ " Liter");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Was moechten Sie tun:");
+            Console.WriteLine("Befuellen(b)");
+            Console.WriteLine("Entnehmen(e)");
+            Console.WriteLine("Beenden(x)");
+            Console.Write(" -->");
+
+        }
+
+
+        public static char Menu(Tank tank)
+        {
+
+            char userEingabe=' ';
+            char[] choiceTable = { 'b', 'e', 'x' };
+            DispLayMenu(tank);
+            bool is_valid = false;
+            do
+            {
+                 userEingabe = Convert.ToChar(Console.Read());
+                 is_valid = Array.Exists(choiceTable, element => element == userEingabe);
+                if (!is_valid)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sil vous plait, Choisissez entre les element suivant:'b', 'e', 'x'. Merci! ");
+                    DispLayMenu(tank);
+                }
+            } while (!is_valid);
+
+            return userEingabe;
+        }
+
+        public static int UserEingabeChecking()
+        {
+            int id = 0;
+            while (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.Write("Please Enter a valid numerical value!");
+               
+            }
+
+            return id;
+        }
+    }
+}
---
+++ TankAufgabe/Tank.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TankAufgabe
+{
+    class Tank
+    {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public int volume { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int inhalt { get; set; }
+
+        public void Befuellen(int userEingabe)
+        {
+            int copyInhalt = this.inhalt;
+            int copyVolume = this.volume;
+            int neueInhalt = copyInhalt + userEingabe;
+
+            while (neueInhalt > this.volume)
+            {
+                userEingabe = 0;
+                int volumeDisponible = copyVolume - copyInhalt;
+                Console.Write("Es pass nur noch "+ volumeDisponible + " hinein");
+                while(!int.TryParse(Console.ReadLine(), out userEingabe))
+                {
+                    Console.WriteLine("Entrez une valeur numric svp!");
+                }
+                neueInhalt = copyInhalt + userEingabe;
+            }
+
+            inhalt += userEingabe;
+            Console.WriteLine("Fuellmenge Tank: "+this.inhalt);
+        }
+
+        public void Entnehmen(int userEingabe)
+        {
+            int copyInhalt = this.inhalt;
+            int neueInhalt = copyInhalt - userEingabe;
+
+            while (neueInhalt<0)
+            {
+                Console.Write("Es sind nur noch "+this.inhalt+" Liter drin!");
+                int.TryParse(Console.ReadLine(), out userEingabe);
+                neueInhalt = copyInhalt - userEingabe;
+            }
+            inhalt -= userEingabe;
+            Console.WriteLine("Fuellmenge Tank: " + this.inhalt);
+        }
+
+    }
+}
